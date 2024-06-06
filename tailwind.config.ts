53ee349a56c0ed82c4fedd6f71a55a752d03d8eb
stormynight9 <ferjani.nader@hotmail.fr>
2024-06-06 15:27:57 +0100

    chore: remove boilerplate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
 export default config;
+
