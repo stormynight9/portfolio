b06d434bfd8cf3013a083e9aa558fa66b5ac0a58
stormynight9 <ferjani.nader@hotmail.fr>
2024-06-19 20:57:05 +0100

    perf: faster image loading

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -24,6 +24,7 @@ const Header = () => {
                                 alt={CONFIG.name + ' avatar'}
                                 width={56}
                                 height={56}
+                                priority={true}
                             />
                         </AvatarImage>
                         <AvatarFallback>
