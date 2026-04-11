e1a5e158d017a5fa42b7e3105c5abf700c51123b
Nader <ferjani.nader@hotmail.fr>
2026-04-11 10:10:34 +0100

    feat: update project section layout and enhance navigation with new button styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,19 @@ export default function Home() {
                 </div>
             </div>
             <div className='animate-slide-from-down-and-fade-3 flex flex-col gap-7'>
-                <h2 className='-mb-4 px-4 font-semibold'>Featured Projects</h2>
+                <div className='-mb-4 flex items-center justify-between gap-4 px-4'>
+                    <h2 className='font-semibold'>Projects I worked on</h2>
+                    <Button
+                        asChild
+                        className='text-muted-foreground hover:text-foreground shrink-0 underline'
+                        variant={'link'}
+                    >
+                        <Link href='/projects'>
+                            All projects
+                            <Icons.arrowUpRight />
+                        </Link>
+                    </Button>
+                </div>
                 {CONFIG.projects
                     .filter((project) => project.featured)
                     .map((project, idx, array) => (
@@ -41,16 +53,18 @@ export default function Home() {
                             )}
                         </React.Fragment>
                     ))}
-                <Button
-                    asChild
-                    className='text-muted-foreground hover:text-foreground ml-auto items-end underline'
-                    variant={'link'}
-                >
-                    <Link href='/projects'>
-                        All projects
-                        <Icons.arrowUpRight className='inline-block size-4' />
-                    </Link>
-                </Button>
+                <div className='flex justify-center'>
+                    <Button
+                        asChild
+                        className='text-muted-foreground hover:text-foreground underline'
+                        variant={'link'}
+                    >
+                        <Link href='/projects'>
+                            More projects
+                            <Icons.arrowUpRight />
+                        </Link>
+                    </Button>
+                </div>
             </div>
 
             <OpenSource />
